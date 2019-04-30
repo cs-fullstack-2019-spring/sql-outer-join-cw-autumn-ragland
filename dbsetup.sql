935337f6910692b640b91c3ee64ee647b7e7cf9b
autumn-ragland <raglanda998@gmail.com>
2019-04-30 15:33:30 -0500

    pm cw push

--- dbsetup.sql
+++ dbsetup.sql
@@ -31,3 +31,44 @@ VALUES
 ('Fred Costner','LENOVA', 3),
 ('Sandra Kilmer','HP', 4),
 ('Julia Mcqueen', NULL, NULL);
+
+-- Create a Join that lists all Employees and their departments
+select employee_name,department_name
+from employees
+left join departments
+on departments.department_id = employees.department_id;
+
+-- Create a Join that lists ONLY Employees assigned to a department
+select employee_name, department_name
+from employees
+left join departments
+on departments.department_id = employees.employee_id
+where department_name notnull;
+
+-- Create a Join that lists all Departments and the Employees
+select department_name,employee_name
+from employees
+right join departments
+on departments.department_id = employees.department_id;
+
+-- Create a Join that lists ONLY Departments that have Employees
+select department_name, employee_name
+from employees
+right join departments
+on departments.department_id = employees.employee_id
+where department_name notnull;
+
+-- Create a Join that lists the number of employees per department
+select department_name, count(employee_name) as "Number of Employees"
+from employees
+right join departments
+on departments.department_id = employees.employee_id
+where department_name notnull
+group by department_name;
+
+-- Create a join that only lists employees assigned to an department who also have a company laptop assigned
+select employee_name, laptop_type, department_name
+from employees
+left join departments
+on employees.department_id = departments.department_id
+where laptop_type notnull and department_name notnull;
